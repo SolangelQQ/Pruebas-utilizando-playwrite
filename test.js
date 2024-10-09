2228faf0e428d4207825b039d3873335948c9425
Jhoselin Quiroga <jhoselin.quiroga@ucb.edu.bo>
2024-10-09 16:05:02 -0400

    agrgando mas timpo para observar resultados

--- test.js
+++ test.js
@@ -1,14 +1,19 @@
 const { chromium } = require('playwright');
 
 (async () => {
-  const browser = await chromium.launch();
-  const context = await browser.newContext();
-  const page = await context.newPage();
-  
-  await page.goto('https://example.com');
-  const title = await page.title();
-  console.log(`El título de la página es: ${title}`);
-  
-  await page.screenshot({ path: 'screenshot.png' });
-  await browser.close();
+  try {
+    const browser = await chromium.launch({ headless: false });
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    
+    await page.goto('https://example.com');
+    const title = await page.title();
+    console.log(`El título de la página es: ${title}`);
+    
+    await page.screenshot({ path: 'screenshot.png' });
+    await page.waitForTimeout(10000);
+    await browser.close();
+  } catch (error) {
+    console.error('Error:', error);
+  }
 })();
